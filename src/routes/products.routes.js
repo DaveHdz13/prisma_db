d52895c63c79ff5edcb040cd7adbbfdeaf13d25c
DaveHdz <codejarHDZ@gmail.com>
2024-01-09 12:49:15 -0600

    Initial commit

---
+++ src/routes/products.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { prisma } from "../db.js";
+
+const router = Router()
+
+router.get('/products', async (req, res) => {
+  const products = await prisma.product.findMany()
+  res.json(products)
+})
+
+router.post('/products', async (req, res) => {
+  const newProduct = await prisma.product.create({
+    data: req.body
+  })
+  res.json(newProduct)
+})
+
+export default router;
